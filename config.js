e122618292279319ae8a2eb22c9d871201d4246a
Brittany Farrell <BFarrell1326@Gmail.Com>
2021-02-20 18:30:46 -0500

    edit config

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8081,
   NODE_ENV: process.env.NODE_ENV,
-  DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
 };
