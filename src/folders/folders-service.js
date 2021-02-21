d1baa537e56ad2329bf4dd2240f8cda2dcfb0896
Brittany Farrell <BFarrell1326@Gmail.Com>
2021-02-21 11:23:54 -0500

    m

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -34,14 +34,14 @@ const FoldersService = {
     console.log(' ');
     console.log(' ');
     console.log(' ');
-    return db
-      .insert(folder)
-      .into('folders')
-      .returning('*')
-      .then(rows => {
-        console.log('row', rows[0]);
-        return rows[0];
-      });
+    return db;
+      // .insert(folder)
+      // .into('folders')
+      // .returning('*')
+      // .then(rows => {
+      //   console.log('row', rows[0]);
+      //   return rows[0];
+      // });
   },
 
   serializeFolder(folder) {
